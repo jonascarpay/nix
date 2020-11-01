1301095fa6e3848b597022007f14f9b4f238c161
Jonas Carpay <jonascarpay@gmail.com>
2020-11-02 04:47:33 +0900

    update agda, plus emacs.init stuff

--- home-modules/agda.nix
+++ home-modules/agda.nix
@@ -1,19 +1,18 @@
 { pkgs, config, ... }: {
-  config = {
-    home = {
-      packages = [ pkgs.haskellPackages.Agda ];
-      file = {
-        ".agda/standard-library.agda-lib".text = ''
-          name: standard-library
-          include: ${pkgs.AgdaStdlib}/share/agda/
-        '';
-        ".agda/libraries".text = ''
-          /home/jmc/.agda/standard-library.agda-lib
-        '';
-        ".agda/defaults".text = ''
-          standard-library
-        '';
-      };
-    };
+  home = {
+    packages = [
+      (pkgs.agda.withPackages (p: [ p.standard-library ]))
+      pkgs.mononoki
+    ];
+    file.".agda/defaults".text = "standard-library";
   };
+  programs.emacs.init.prelude = ''
+    (load-file (let ((coding-system-for-read 'utf-8))
+                    (shell-command-to-string "agda-mode locate")))
+    (set-face-attribute 'default nil
+      :family "mononoki"
+      :height 120
+      :weight 'normal
+      :width 'normal)
+  '';
 }
