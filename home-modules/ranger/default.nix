2ec7135c9a896028c6412c184917663fc68b5e6b
Jonas Carpay <jonascarpay@gmail.com>
2021-01-01 02:31:16 +0900

    Fix ranger xdg-open

--- home-modules/ranger/default.nix
+++ home-modules/ranger/default.nix
@@ -1,11 +1,25 @@
 { pkgs, ... }:
-
 {
 
   home.packages = [ pkgs.ranger ];
-  home.file.rangerConf = {
-    target = ".config/ranger/rc.conf";
-    source = ./rc.conf;
-  };
 
+  # see https://github.com/ranger/ranger/issues/1853
+  xdg.configFile.rifle = {
+    target = "ranger/rifle.conf";
+    source =
+      let
+        confPatch = pkgs.writeText "patch" ''
+          265c265
+          < label open, has xdg-open = xdg-open -- "$@"
+          ---
+          > label open, has xdg-open = xdg-open "$@"
+        '';
+
+        conf = pkgs.runCommand "rifle-conf" { } ''
+          ${pkgs.ranger}/bin/ranger --copy-config=rifle --confdir=$TMPDIR
+          patch $TMPDIR/rifle.conf -o $out < ${confPatch}
+        '';
+      in
+      conf;
+  };
 }
