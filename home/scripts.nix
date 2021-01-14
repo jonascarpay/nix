81b25684b0079a9983ee16a2aa5874d15cfad4b7
Jonas Carpay <jonascarpay@gmail.com>
2021-01-14 18:44:57 +0900

    add zap, remove unused scripts

--- home/scripts.nix
+++ home/scripts.nix
@@ -3,15 +3,6 @@ with lib;
 let
   scripts = {
     vimdir = ''
-      #!/usr/bin/env bash
-      set -e
-
-      [[ -n "$1" ]] && cd "$1"
-      FILE=$(fzf --preview='cat {+}')
-      vim "$FILE"
-    '';
-    vimgrep = ''
-      #!/usr/bin/env bash
       set -e
 
       [[ -n "$1" ]] && cd "$1"
@@ -19,18 +10,13 @@ let
       vim "$FILE"
     '';
     nix-fancy-uninstall = ''
-      #!/usr/bin/env bash
       nix-env -q | fzf | xargs -I{} nix-env -e {}
     '';
     stack-clean-all = ''
-      #!/usr/bin/env bash
       find Dev -type d -name ".stack-work" -exec rm -rf {} \;
     '';
-    nix-buildlog = ''
-      #!/usr/bin/env bash
-      if !(nix build $@ 2> /dev/null); then
-        nix log $@
-      fi
+    zap = ''
+      xprop | grep -oP "PID\(CARDINAL\) = \K\d+" | xargs kill -9
     '';
   };
 
