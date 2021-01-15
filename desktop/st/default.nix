53b78048e4424f09ff0727cb32f564eed40087a4
Jonas Carpay <jonascarpay@gmail.com>
2021-01-15 23:15:30 +0900

    ST stuff

--- desktop/st/default.nix
+++ desktop/st/default.nix
@@ -1,40 +1,43 @@
 { pkgs, ... }:
 let
-  inherit (pkgs) fetchpatch;
+  inherit (pkgs) fetchurl;
+  unstable = import <unstable> { };
   configFile = pkgs.writeText "config.def.h" (builtins.readFile ./config.h);
-  myst = pkgs.st.overrideAttrs (old: {
+  myst = unstable.st.overrideAttrs (old: {
+    buildInputs = old.buildInputs ++ [ pkgs.harfbuzz ];
     patches = old.patches ++ [
       # Scrollback
-      (fetchpatch {
+      (fetchurl {
         url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.8.4.diff";
-        sha256 = "0valvkbsf2qbai8551id6jc0szn61303f3l6r8wfjmjnn4054r3c";
+        sha256 = "0i0fav13sxnsydpllny26139gnzai66222502cplh18iy5fir3j1";
       })
       # Shift-mousewheel scroll
-      (fetchpatch {
+      (fetchurl {
         url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-20191024-a2c479c.diff";
-        sha256 = "0qg20sv64im5lcnfnphnbbiyizwywrg1g6zhxyxqqyf8g33lpbb7";
+        sha256 = "0z961sv4pxa1sxrbhalqzz2ldl7qb26qk9l11zx1hp8rh3cmi51i";
       })
-      # mousehweel scroll (patch does not apply?)
-      # (fetchpatch {
-      #   url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-altscreen-20200416-5703aa0.diff";
-      #   sha256 = "0j13prfvv9mjvyjiklachipzhjdradb1dymbmx7fk4d083il06m6";
-      # })
+      # non-shift mousewheel scroll (needs previous 2)
+      ./mousewheelincrement.diff
+
+      (fetchurl {
+        url = "https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff";
+        sha256 = "0ssj7gsb3snk1pqfkffwc0dshrbmvf7ffqvrdi4k2p451mnqmph1";
+      })
+      (fetchurl {
+        url = "https://st.suckless.org/patches/workingdir/st-workingdir-20200317-51e19ea.diff";
+        sha256 = "1pwx6gppqbx0gkyw62i1qwpigr9nw0chnzr0f3jylv9gzxyvxfhy";
+      })
+      (fetchurl {
+        url = "https://st.suckless.org/patches/w3m/st-w3m-0.8.3.diff";
+        sha256 = "1cwidwqyg6qv68x8bsnxns2h0gy9crd5hs2z99xcd5m0q3agpmlb";
+      })
+      (fetchurl {
+        url = "https://st.suckless.org/patches/ligatures/0.8.3/st-ligatures-scrollback-20200430-0.8.3.diff";
+        sha256 = "0c7g3wcacxlrs7v0j2drgqg2wksggjicsym6pqawca8fi15bkbfq";
+      })
 
       ./font.diff
       ./zoom.diff
-      (fetchpatch {
-        url = "https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff";
-        sha256 = "03b709p3mywydzsi5gx8j24krb63kzm5if4dyqq7s0ahh85qiv7n";
-      })
-      (fetchpatch {
-        url = "https://st.suckless.org/patches/workingdir/st-workingdir-20200317-51e19ea.diff";
-        sha256 = "1sqzm920vn8hr3p8mv7p9kflzfksks59454yldw8bryvqrgspdzi";
-      })
-      (fetchpatch {
-        url = "https://st.suckless.org/patches/w3m/st-w3m-0.8.3.diff";
-        sha256 = "12gnp4hsidyps2n2cqscac3r2jyanxpwxp998057mdwi7grqcm4x";
-      })
-
     ];
   });
 in
