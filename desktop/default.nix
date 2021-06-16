33f08a9bf744b86246ea9e71737a15418538749d
Jonas Carpay <jonascarpay@gmail.com>
2021-06-16 18:31:16 +0900

    microrice dunst

--- desktop/default.nix
+++ desktop/default.nix
@@ -46,14 +46,16 @@
         colors.foreground = "#d8dee9";
         global = {
           follow = "keyboard";
-          geometry = "0-15+45"; # tweaked for bar size 30
+          geometry = "900-15+45"; # tweaked for bar size 30
+          # shrink = true; # i want this but width calculation is broken? spotify notifications get line breaks for some reason
           padding = 8; # vertical
           horizontal_padding = 8;
           text_icon_padding = 16;
           markup = "full";
           font = "SauceCodePro Nerd Font 10";
-          format = "%s\n%b";
+          format = "<b>%s</b>\\n\\n%b";
           max_icon_size = 160;
+          word_wrap = true;
         };
         urgency_low.background = "#232831";
         urgency_normal.background = "#232831";
