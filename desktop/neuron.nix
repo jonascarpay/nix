6f1742c6ae951865cc99811b11acf5e5fe501d33
Jonas Carpay <jonascarpay@gmail.com>
2021-07-04 15:48:32 +0900

    Neuron auto-sync

--- desktop/neuron.nix
+++ desktop/neuron.nix
@@ -7,18 +7,35 @@ let
   notesDir = "/home/jmc/slipbox";
 in
 {
-  home.packages = [ pkgs.ispell ];
-  systemd.user.services.neuron = {
-    Unit.Description = "Neuron zettelkasten service";
-    Install.WantedBy = [ "graphical-session.target" ];
-    Service.ExecStart = "${neuronBin} -d ${notesDir} gen -ws :${port}";
+  systemd.user = {
+    services.neuron = {
+      Unit.Description = "Neuron zettelkasten service";
+      Install.WantedBy = [ "graphical-session.target" ];
+      Service.ExecStart = "${neuronBin} -d ${notesDir} gen -ws :${port}";
+    };
+    services.neuron-sync = {
+      Unit.Description = "Neuron github sync";
+      Service = {
+        Type = "oneshot";
+        ExecStart =
+          let script = pkgs.writeShellScript "neuron-sync" ''
+            cd ${notesDir}
+            ${pkgs.gitAndTools.git-sync}/bin/git-sync
+          '';
+          in "${script}";
+      };
+    };
+    timers.neuron-sync = {
+      Unit.Description = "Neuron sync timer";
+      Timer.OnCalendar = "*:0/15";
+      Install.WantedBy = [ "timers.target" ];
+    };
   };
-
   programs.emacs.init.modules.neuron-mode.config = ''
     (use-package neuron-mode
       :after general
       :config
-      (setq neuron-default-zettelkasten-directory "~/slipbox")
+      (setq neuron-default-zettelkasten-directory ${notesDir})
       (setq neuron-executable "${neuronBin}")
       (setq neuron-rib-server-port ${port})
       (setq-default markdown-hide-markup t)
@@ -30,13 +47,4 @@ in
       )
     )
   '';
-  # programs.neovim.init.modules = _: {
-  #   neuron = {
-  #     plugins = [ (import <unstable> { }).vimPlugins.neuron-vim ];
-  #     config = ''
-  #       let g:neuron_dir = "${notesDir}/"
-  #       let g:neuron_executable = "${neuronBin}"
-  #     '';
-  #   };
-  # };
 }
