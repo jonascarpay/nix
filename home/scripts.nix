8ca0510c9506825d1e4f9c4fe2ef8a82d856f284
Jonas Carpay <jonascarpay@gmail.com>
2021-07-19 13:38:39 +0900

    nix-bump script

--- home/scripts.nix
+++ home/scripts.nix
@@ -16,6 +16,13 @@ let
     zap = ''
       xprop | grep -oP "PID\(CARDINAL\) = \K\d+" | xargs kill -9
     '';
+    nix-bump = ''
+      shopt -s globstar
+      for f in **/*.nix; do
+        printf "$f:\t"
+        ${pkgs.update-nix-fetchgit}/bin/update-nix-fetchgit -v $f
+      done
+    '';
   };
 
 in
