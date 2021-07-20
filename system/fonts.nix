8ed5a03bc8b185c3ee5255439b2cb3874ffe17e1
Jonas Carpay <jonascarpay@gmail.com>
2021-07-21 02:58:16 +0900

    dm-mono: just pull in all of google fonts instead

--- system/fonts.nix
+++ system/fonts.nix
@@ -1,28 +1,10 @@
 { pkgs, ... }:
-let
-  dm-mono = pkgs.stdenv.mkDerivation {
-    name = "dm-mono";
-    src = pkgs.fetchFromGitHub {
-      owner = "googlefonts";
-      repo = "dm-mono";
-      rev = "57fadabfb200a77de2812540026c249dc3013077";
-      sha256 = "07y3csk0vy3b3mq33bb73m63p9vyk8dhf6ysqnxabdpvy6d98gjy";
-    };
-    buildPhase = "true";
-    installPhase = ''
-      mkdir -p $out/share/doc/dm-mono
-      mkdir -p $out/fonts/truetype
-      cp AUTHORS.txt CHANGELOG.md $out/share/doc/dm-mono
-      cp exports/* $out/fonts/truetype
-    '';
-  };
-in
 {
   fonts = {
     enableDefaultFonts = true;
     fonts = with pkgs; [
       corefonts
-      dm-mono
+      google-fonts
       liberation_ttf
       nerdfonts
       powerline-fonts
