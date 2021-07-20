940102af9a8b281956d4fbc860d607ae84a18e5c
Jonas Carpay <jonascarpay@gmail.com>
2021-07-21 01:08:31 +0900

    neovim: compe completion

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -185,7 +185,7 @@ in
         };
 
         lsp = {
-          plugins = [ unp.nvim-lspconfig ];
+          plugins = [ unp.nvim-lspconfig unp.nvim-compe ];
           config = ''
             lua << EOF
               require'lspconfig'.pyright.setup{}
@@ -208,6 +208,66 @@ in
             nn <leader>lk <cmd> lua vim.lsp.buf.signature_help()<cr>
             nn [l <cmd> lua vim.lsp.diagnostic.goto_prev()<cr>
             nn ]l <cmd> lua vim.lsp.diagnostic.goto_next()<cr>
+            lua << EOF
+            -- Compe setup
+            require'compe'.setup {
+              enabled = true;
+              autocomplete = true;
+              debug = false;
+              min_length = 1;
+              preselect = 'enable';
+              throttle_time = 80;
+              source_timeout = 200;
+              incomplete_delay = 400;
+              max_abbr_width = 100;
+              max_kind_width = 100;
+              max_menu_width = 100;
+              documentation = true;
+
+              source = {
+                path = true;
+                nvim_lsp = true;
+              };
+            }
+
+            local t = function(str)
+              return vim.api.nvim_replace_termcodes(str, true, true, true)
+            end
+
+            local check_back_space = function()
+                local col = vim.fn.col('.') - 1
+                if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                    return true
+                else
+                    return false
+                end
+            end
+
+            -- Use (s-)tab to:
+            --- move to prev/next item in completion menuone
+            --- jump to prev/next snippet's placeholder
+            _G.tab_complete = function()
+              if vim.fn.pumvisible() == 1 then
+                return t "<C-n>"
+              elseif check_back_space() then
+                return t "<Tab>"
+              else
+                return vim.fn['compe#complete']()
+              end
+            end
+            _G.s_tab_complete = function()
+              if vim.fn.pumvisible() == 1 then
+                return t "<C-p>"
+              else
+                return t "<S-Tab>"
+              end
+            end
+
+            vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+            EOF
           '';
         };
 
