79cd83762644445e64d27b497d1b870bfc1e4727
Jonas Carpay <jonascarpay@gmail.com>
2021-07-22 03:06:51 +0900

    zfs: don't mount during boot

--- system/zfs.nix
+++ system/zfs.nix
@@ -2,6 +2,7 @@ hostId: {
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = hostId;
   boot.zfs.extraPools = [ "tank" ];
+  boot.zfs.requestEncryptionCredentials = false;
   services.zfs.autoScrub.enable = true;
   services.zfs.autoSnapshot.enable = true;
 }
