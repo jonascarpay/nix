b16651492cca05dd9409d6bf19b5a803dd323422
Jonas Carpay <jonascarpay@gmail.com>
2021-07-21 11:24:39 +0900

    emacs: fix solaire-mode config

--- desktop/emacs/ui.nix
+++ desktop/emacs/ui.nix
@@ -11,30 +11,17 @@
     solaire-mode.config = ''
       ;; A more complex, more lazy-loaded config
       (use-package solaire-mode
-        :after doom-themes
-        ;; Ensure solaire-mode is running in all solaire-mode buffers
-        :hook (change-major-mode . turn-on-solaire-mode)
-        ;; ...if you use auto-revert-mode, this prevents solaire-mode from turning
-        ;; itself off every time Emacs reverts the file
-        :hook (after-revert . turn-on-solaire-mode)
-        ;; To enable solaire-mode unconditionally for certain modes:
-        :hook (ediff-prepare-buffer . solaire-mode)
-        ;; Highlight the minibuffer when it is activated:
-        :hook (minibuffer-setup . solaire-mode-in-minibuffer)
         :config
-        ;; The bright and dark background colors are automatically swapped the first 
-        ;; time solaire-mode is activated. Namely, the backgrounds of the `default` and
-        ;; `solaire-default-face` faces are swapped. This is done because the colors 
-        ;; are usually the wrong way around. If you don't want this, you can disable it:
-        (setq solaire-mode-auto-swap-bg nil)
-
         (solaire-global-mode +1)
-        (load-theme 'doom-nord t)
       )
     '';
 
     doom-themes.config = ''
-      (use-package doom-themes)
+      (use-package doom-themes
+        :after solaire-mode
+        :config
+        (load-theme 'doom-nord t)
+      )
     '';
 
     doom-modeline.config = ''
