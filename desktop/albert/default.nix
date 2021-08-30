ab6d9bbb518aeed98510e9300e9d037622b8ef49
Jonas Carpay <jonascarpay@gmail.com>
2021-08-30 21:21:14 +0900

    albert: add books to search path

--- desktop/albert/default.nix
+++ desktop/albert/default.nix
@@ -3,8 +3,7 @@
   home.file = {
     albertConf.target = ".config/albert/albert.conf";
     albertConf.source = ./albert.conf;
-    engines.target =
-      ".config/albert/org.albert.extension.websearch/engines.json";
+    engines.target = ".config/albert/org.albert.extension.websearch/engines.json";
     engines.source = ./engines.json;
   };
   xsession.initExtra = ''
