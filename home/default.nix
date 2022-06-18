d34011a2d8a9ee7430896be8061b95e53b608a9d
Jonas Carpay <jonascarpay@gmail.com>
2022-06-18 23:42:17 +0900

    home: drop z-lua

--- home/default.nix
+++ home/default.nix
@@ -61,13 +61,6 @@
     man.generateCaches = true;
     fzf.enable = true;
 
-    z-lua = {
-      enable = true;
-      options = [ "enhanced" "once" "fzf" ];
-      enableAliases = true;
-      # https://github.com/nix-community/home-manager/blob/master/modules/programs/z-lua.nix#L9
-    };
-
     git = {
       enable = true;
       userName = "Jonas Carpay";
