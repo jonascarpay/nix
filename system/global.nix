d6ad3bc500b9b539dbb5c260deca867f35915512
Jonas Carpay <jonascarpay@gmail.com>
2022-06-25 18:34:56 +0200

    global: Re-enable documentation

--- system/global.nix
+++ system/global.nix
@@ -114,7 +114,7 @@ in
     users.root.hashedPassword = "$6$m26D1J9rYXRcv.UX$sMAEzl.q5Bj2GrvJ1S5t09rCS499dQeuL08blpLl3ZdjTNKoD5a7.SkQUZwVMkF5Ui8GWoR26O2kdSdRnyPfK1";
   };
   documentation = {
-    nixos.enable = false;
+    nixos.enable = true;
     # nixos.includeAllModules = true;
     man.generateCaches = true; # Needed for fish man page completions
     dev.enable = true;
