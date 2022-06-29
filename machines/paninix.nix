d37e61a9e6b4426fda7534ec2a6bf852f4dc38ad
Jonas Carpay <jonascarpay@gmail.com>
2022-06-29 23:15:58 +0200

    paninix wireguard: only route 192.168.1.* through wg

--- machines/paninix.nix
+++ machines/paninix.nix
@@ -11,7 +11,7 @@ let
         privateKeyFile = config.age.secrets.wg-paninix.path;
         peers = [{
           publicKey = "1DQu3fqcOew1Mxvpiq/0umajstSXEzdcfhY89dcHkHw=";
-          allowedIPs = [ "0.0.0.0/0" ];
+          allowedIPs = [ "192.168.1.0/24" ]; # Only route internal traffic through wg
           endpoint = "126.51.120.49:${builtins.toString port}";
           persistentKeepalive = 25;
         }];
