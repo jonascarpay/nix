d74264d0372546b2011fc51675393c943728de69
Jonas Carpay <jonascarpay@gmail.com>
2022-07-04 15:21:11 +0200

    ranger: fix images on st

--- home/ranger.nix
+++ home/ranger.nix
@@ -1,25 +1,27 @@
 { pkgs, ... }:
 {
 
-  home.packages = [ pkgs.ranger ];
+  home.packages = [
+    pkgs.ranger
+    pkgs.python3Packages.ueberzug
+  ];
 
   # see https://github.com/ranger/ranger/issues/1853
-  xdg.configFile.rifle = {
-    target = "ranger/rifle.conf";
-    source =
-      let
-        confPatch = pkgs.writeText "patch" ''
-          265c265
-          < label open, has xdg-open = xdg-open -- "$@"
-          ---
-          > label open, has xdg-open = xdg-open "$@"
-        '';
-
-        conf = pkgs.runCommand "rifle-conf" { } ''
-          ${pkgs.ranger}/bin/ranger --copy-config=rifle --confdir=$TMPDIR
-          patch $TMPDIR/rifle.conf -o $out < ${confPatch}
-        '';
-      in
-      conf;
-  };
+  xdg.configFile."ranger/rifle.conf".source =
+    let
+      confPatch = pkgs.writeText "patch" ''
+        265c265
+        < label open, has xdg-open = xdg-open -- "$@"
+        ---
+        > label open, has xdg-open = xdg-open "$@"
+      '';
+    in
+    pkgs.runCommand "rifle-conf" { } ''
+      ${pkgs.ranger}/bin/ranger --copy-config=rifle --confdir=$TMPDIR
+      patch $TMPDIR/rifle.conf -o $out < ${confPatch}
+    '';
+  xdg.configFile."ranger/rc.conf".text = ''
+    set preview_images true
+    set preview_images_method ueberzug
+  '';
 }
