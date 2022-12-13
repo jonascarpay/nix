dc731bfb37e932e93b2d7faeda56bb4bf827d371
Jonas Carpay <jonascarpay@gmail.com>
2022-12-13 14:28:02 +0900

    vim: nvim-cmp completion

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -249,6 +249,7 @@ in
         '';
       }
 
+      np.cmp-nvim-lsp
       {
         plugin = np.nvim-lspconfig;
         # https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
@@ -290,13 +291,64 @@ in
             vim.keymap.set('n', '<space>lt', function() vim.lsp.buf.format { async = true } end, bufopts)
           end
 
-          require'lspconfig'.hls.setup{on_attach = on_attach}
+          local lspconfig = require('lspconfig')
+          local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+          lspconfig.hls.setup{
+            on_attach = on_attach,
+            capabilities = cmp_capabilities,
+          }
+          lspconfig.pyright.setup{
+            on_attach = on_attach,
+            capabilities = cmp_capabilities,
+          }
           EOF
         '';
       }
 
+      # https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion#nvim-cmp
+      {
+        plugin = np.nvim-cmp;
+        config = ''
+          lua << EOF
+          local cmp = require('cmp')
+          cmp.setup {
+            -- snippet ...
+            mapping = cmp.mapping.preset.insert({
+              ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<CR>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+              },
+              ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                else
+                  fallback()
+                end
+              end, { 'i', 's' }),
+              ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { 'i', 's' }),
+            }),
+            sources = {
+              { name = 'nvim_lsp' },
+              -- { name = 'luasnip' },
+            },
+          }
+          EOF
+        '';
+      }
 
     ];
-
   };
 }
