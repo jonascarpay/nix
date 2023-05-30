51dc011f30b9204b5c9f35b507555b5f854333ee
Jonas Carpay <jonascarpay@gmail.com>
2023-05-30 16:07:19 +0900

    picom: exclude fcitx window shadow

--- desktop/picom.nix
+++ desktop/picom.nix
@@ -14,6 +14,8 @@
       inactive-dim = 0.20;
     };
     # shadowExclude = [ "class_g = 'i3-frame'" ];
+    shadowExclude = [ "class_g = 'fcitx'" ];
+
     opacityRules = [ "100:class_i != 'st-256color'" ];
   };
 }
