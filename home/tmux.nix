ec879059bf5239295f52e30a01a42f4f836771fb
Jonas Carpay <jonascarpay@gmail.com>
2024-02-05 22:59:34 +0900

    tmux: fix hsplit

--- home/tmux.nix
+++ home/tmux.nix
@@ -6,7 +6,7 @@
       run-shell ${pkgs.tmuxPlugins.sensible}/share/tmux-plugins/sensible/sensible.tmux
       run-shell ${pkgs.tmuxPlugins.pain-control}/share/tmux-plugins/pain-control/pain_control.tmux
       set -g mouse on
-      bind v split-window -h
+      bind v split-window
       bind V split-window -v
     '';
   };
