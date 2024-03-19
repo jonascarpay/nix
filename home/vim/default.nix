9323a44ec12e6f108de844c19118b190ae794a27
Jonas Carpay <jonascarpay@gmail.com>
2024-03-19 10:18:14 +0900

    vim: add signature

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -270,6 +270,7 @@ in
         '';
       }
 
+      np.vim-signature
       np.vim-polyglot # only used for proper indent on cc
       np.commentary # TODO switch to Comment.nvim
       np.surround
