4a31857444f22c474fcd8c18bf3477dffeaa9595
Jonas Carpay <jonascarpay@gmail.com>
2025-03-04 12:27:43 +0900

    global: tcl access

--- nixos/global.nix
+++ nixos/global.nix
@@ -129,6 +129,7 @@ in
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDOlH/zVT9nxSmWA2xcenBhHg0+21WtYV3ZugYWopq1CKdHuPKcgbJ5mq3yvQaWEDJu/bxeuj98F2jJVALJRuVk215GQQxBZlUn3HQefEO5wxpL9lb64B/ElBcoMLBbD3PqdXGvd2QEwOnJvDB+AJY5AtgjEdAvxgfeta/Yd4bmKlSC2Gq6UbpM3zAfN4gH1wRnUm22hTpEsyPVygfu0vfwSY54/DtAXoK7t/6WseLEcIfO/1KD0KZRH+7KDXh3prAV4MwtrNItLGUXDoUzUbF+da/5NHQG+bQrNQiubQJzoGPvo7ecDfvwLDQwBbzS2+OIBQZhP171cQyTmPU0xzwts2cF8XIcJpLVdmU36zHUXxCQIBDzxqAqjLGeh8DbK/e04tlmtfMw0xygUyXI9rXxOFoIy3c5DTDfveYj4G22W2WG+3zX1LznFYNXQLjlsiEheBzt0utC4zbrL7lc0SEihni4yajeWOcCElBBI57NDspI2HtgVXPI15NzbuNM1WCiz6Hq9xtTgums5n2K6bIKFvvqvhMBbJctBFqgthG3j24J/ajK6ZhCl5aqsxr6tRKr0M5qvAGnu0cNxAhmVkx2L7gOopxccVRuPx91moDOvPb185SWf/zL0NSkiSNhuO2lMZ9XjxMOf+VShSvEPu8mTksy0GumhxEjmXx4yhhAnw== jonascarpay@gmail.com"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJog1Go1KtITOMM1SNk1yqZ+S7F03/+WGd8H6mYBra8M ipro"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILyj3zPZyLZp/VgNwem7rBhqYr0iVSJsZGIEZ/HU/kMw tcl@cedbook.local"
       ];
       # mkpasswd -m sha-512
       hashedPassword = "$6$YfPTTLclMnT7Pz.9$7UkmuSo9QHs4rhWDZ.8RRwGNRgkc6Nx8/L11A9.wcWBwuCdQyCgjZj0BB6Kc2AavDAtlT125o5wNlvzv2mrcJ0";
