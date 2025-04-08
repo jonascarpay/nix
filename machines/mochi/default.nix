dd59aaf33b87e2275a0835ba3cda39532a86ccee
Jonas Carpay <jonascarpay@gmail.com>
2025-04-08 16:55:16 +0900

    mochi: drop extraLocaleSettings

--- machines/mochi/default.nix
+++ machines/mochi/default.nix
@@ -138,18 +138,6 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
 
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ja_JP.UTF-8";
-    LC_IDENTIFICATION = "ja_JP.UTF-8";
-    LC_MEASUREMENT = "ja_JP.UTF-8";
-    LC_MONETARY = "ja_JP.UTF-8";
-    LC_NAME = "ja_JP.UTF-8";
-    LC_NUMERIC = "ja_JP.UTF-8";
-    LC_PAPER = "ja_JP.UTF-8";
-    LC_TELEPHONE = "ja_JP.UTF-8";
-    LC_TIME = "ja_JP.UTF-8";
-  };
-
 
   system.stateVersion = "23.11";
 }
