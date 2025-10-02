3cb5abf787790f3f36c02a50a8c142bf937ae40f
Jonas Carpay <jonascarpay@gmail.com>
2025-10-02 22:52:29 +0900

    vim: oil git stuff

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -106,6 +106,7 @@ let
   oil.programs.neovim = {
     plugins = [
       np.oil-nvim
+      np.oil-git-status-nvim
       np.mini-icons
       np.nvim-web-devicons
     ];
@@ -113,13 +114,101 @@ let
       :set splitright
       :set splitbelow
     '';
-    extraLuaConfig = ''
+    extraLuaConfig = /* lua */ ''
+      ---- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#hide-gitignored-files-and-show-git-tracked-hidden-files
+      -- helper function to parse output
+      local function parse_output(proc)
+        local result = proc:wait()
+        local ret = {}
+        if result.code == 0 then
+            for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+            -- Remove trailing slash
+            line = line:gsub("/$", "")
+            ret[line] = true
+          end
+        end
+        return ret
+        end
+
+      -- build git status cache
+      local function new_git_status()
+        return setmetatable({}, {
+          __index = function(self, key)
+            local ignore_proc = vim.system(
+              { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+              {
+                cwd = key,
+                text = true,
+              }
+            )
+            local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+              cwd = key,
+              text = true,
+            })
+            local ret = {
+              ignored = parse_output(ignore_proc),
+              tracked = parse_output(tracked_proc),
+            }
+
+            rawset(self, key, ret)
+            return ret
+          end,
+        })
+      end
+      local git_status = new_git_status()
+
+      -- Clear git status cache on refresh
+      local refresh = require("oil.actions").refresh
+      local orig_refresh = refresh.callback
+      refresh.callback = function(...)
+        git_status = new_git_status()
+        orig_refresh(...)
+      end
+      local function git_hidden_file(name, bufnr)
+        local dir = require("oil").get_current_dir(bufnr)
+        local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+        -- if no local directory (e.g. for ssh connections), just hide dotfiles
+        if not dir then
+          return is_dotfile
+        end
+        if is_dotfile then
+          return not git_status[dir].tracked[name]
+        else
+          return git_status[dir].ignored[name]
+        end
+      end
+      ----
+
       require("oil").setup({
         default_file_explorer = true,
         columns = { "icon" },
+        win_options = {
+          signcolumn = "yes:2", -- For oil-git-status-nvim
+        },
         view_options = {
           natural_order = "fast",
-        }
+          is_hidden_file = git_hidden_file,
+        },
+      })
+      local git_symbols = {
+        [" "] = " ", -- Unmodified
+        ["!"] = "", -- Ignored: nf-mdi-dots_horizontal
+        ["?"] = "", -- Untracked: nf-fa-question_circle
+        ["A"] = "", -- Added: nf-fa-plus
+        ["C"] = "", -- Copied: nf-fa-copy
+        ["D"] = "", -- Deleted: nf-fa-minus
+        ["M"] = "", -- Modified: nf-mdi-circle_medium
+        ["R"] = "", -- Renamed: nf-fa-arrow_right
+        ["T"] = "", -- TypeChanged: nf-fa-info_circle
+        ["U"] = "", -- Unmerged: nf-fa-exclamation_triangle
+      }
+
+      require('oil-git-status').setup({
+        show_ignored = true,
+        symbols = {
+          index = git_symbols,
+          working_tree = git_symbols,
+        },
       })
       vim.api.nvim_set_keymap('n', '<C-n>', ':Oil<CR>', { noremap = true, })
       -- Sadly, oil on startup causes more problems than it's worth
